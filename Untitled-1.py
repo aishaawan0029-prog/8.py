1339881748035b0106bbeffcae1d9a04f1dcde69
aishaawan0029-prog <aishaawan0029@gmail.com>
2026-03-08 13:38:12 +0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+Maths=int(input("Write your maks in Maths"))
+Engish=int(input("Write your maks in English"))
+Science=int(input("Write your maks in Science"))
+Urdu=int(input("Write your maks in Urdu"))
+sum=Maths+Engish+Science+Urdu
+print(sum)
+perc=sum/400*100
+print(perc)
